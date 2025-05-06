aaac2e95292074c55e7b11a328d9403d0c3c204c
nikita <nikitakadam2293@gmail.com>
2025-05-06 18:00:29 +0530

    updated code

--- collectionframework/comparableandcomparator/ComparatorSorting.java
+++ collectionframework/comparableandcomparator/ComparatorSorting.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class EmployeeComparator
 {
@@ -92,6 +93,40 @@ public class ComparatorSorting {
         employeeList.add(emp3);
         employeeList.add(emp4);
         employeeList.add(emp5);
+
+        System.out.println("_______________________________");
+
+        // First name of all employee
+
+        List<String> firstName1 =  employeeList.stream().map(employee->employee.getFirstName()).collect(Collectors.toList());
+        System.out.println("List of First Name : " + firstName1);
+
+        List<String> fn = employeeList.stream().map(EmployeeComparator::getFirstName).collect(Collectors.toList());
+        System.out.println("Get first name using method reference :  " + fn);
+
+        System.out.println("+++++++++++++++++++++++++");
+
+        List<Double> salary = employeeList.stream().map(employee->employee.getSalary()).collect(Collectors.toList());
+        System.out.println("Salary is : " + salary);
+
+       // get salary using collection
+
+        List<Double> slCollection = new ArrayList<>();
+        for (EmployeeComparator sl : employeeList )
+        {
+            slCollection.add(sl.getSalary()) ;
+        }
+
+        System.out.println("Print Salary using Collection : " + slCollection);
+
+        System.out.println("+++++++++++++++++++++++++");
+
+        List<Double> sl = employeeList.stream().map(EmployeeComparator::getSalary).collect(Collectors.toList());
+        System.out.println("Get Salary using method reference : " + sl);
+
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("_______________________________");
+
 
         System.out.println("Before sorting : "+ employeeList );
 
